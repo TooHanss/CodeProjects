6afff7a7143dc923c913ea5df842169ed197f860
TomHanssens <91314727+TooHanss@users.noreply.github.com>
2024-11-24 23:01:51 +1100

    added orientation to movement directon

--- Boids/boid/boid.py
+++ Boids/boid/boid.py
@@ -1,5 +1,9 @@
 import pygame
-
+import math
+# Need to store image when rotatin or when it blits it is the unrotated version
+# Calculate velocity
+# Orient to velocity
+# Screen Wrap
 IMAGE_PATH = r'Boids\boid\image\Boid.png'
 
 class Boid:
@@ -8,9 +12,28 @@ class Boid:
         self.scale = (0.1, 0.1)
         pygame.transform.scale(self.img, self.scale)
         self.pos = (100, 100)
+        self.speed = (1.0)
+        self.dir = (1.0, 1.0)
+        self.angle = 0
 
     def draw(self, screen):
         screen.blit(self.img, self.pos)
 
     def move(self):
-        self.pos = (self.pos[0], self.pos[1] + 1)
+        self.pos = (self.pos[0] + self.dir[0], self.pos[1] + self.dir[1])
+
+    def update(self, screen):
+        self.move()
+        self.update_angle()
+        self.draw(screen=screen)
+        
+
+    def update_angle(self):
+        magnitude = math.sqrt(self.dir[0]*2 + self.dir[1]*2)
+        mag_x = self.dir[0] / magnitude
+        mag_y = self.dir[1] / magnitude
+
+        # Calculate the angle (in degrees)
+        self.angle = math.degrees(math.atan2(-mag_x, mag_y)) - 90
+        print(self.angle)
+        pygame.transform.rotate(self.img, self.angle)
--- Boids/main.py
+++ Boids/main.py
@@ -1,7 +1,9 @@
 import pygame
+import math
 from boid.boid import Boid
 pygame.init()
 
+IMAGE_PATH = r'Boids\boid\image\Boid.png'
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
@@ -9,10 +11,24 @@ BOID = Boid()
 
 clock = pygame.time.Clock()
 run = True
+pos = (100.0, 100.0)
 while run:
-    screen.fill((0,0,0))
-    BOID.draw(screen=screen)
-    BOID.move()
+    screen.fill((0, 0, 0))
+    dx = 1.0
+    dy = 1.0
+    img = pygame.image.load(IMAGE_PATH).convert()
+    
+    magnitude = math.sqrt(dx**2 + dy**2)
+    dx /= magnitude
+    dy /= magnitude
+
+    # Calculate the angle (in degrees)
+    angle = math.degrees(math.atan2(-dy, dx)) - 90
+    
+    img = pygame.transform.rotate(img, angle)
+
+    pos = (pos[0] + dx, pos[1] + dy)
+    screen.blit(img, (pos))
     pygame.display.flip()
     clock.tick(30)
 
