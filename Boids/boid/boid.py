2fd44af755c54506b79366cacb353de2ad5ffeb9
TomHanssens <91314727+TooHanss@users.noreply.github.com>
2024-11-25 22:29:22 +1100

    got alignment workin

--- Boids/boid/boid.py
+++ Boids/boid/boid.py
@@ -1,39 +1,64 @@
 import pygame
 import math
+import random
 # Need to store image when rotatin or when it blits it is the unrotated version
 # Calculate velocity
 # Orient to velocity
 # Screen Wrap
-IMAGE_PATH = r'Boids\boid\image\Boid.png'
 
 class Boid:
-    def __init__(self):
-        self.img = pygame.image.load(IMAGE_PATH).convert()
-        self.scale = (0.1, 0.1)
-        pygame.transform.scale(self.img, self.scale)
-        self.pos = (100, 100)
-        self.speed = (1.0)
-        self.dir = (1.0, 1.0)
+    def __init__(self, screen: pygame.Surface, school):
+        self.screen = screen
+        self.school = school
+        self.pos = pygame.Vector2(random.randint(0, screen.get_width()), random.randint(0, screen.get_height())) 
+        self.max_speed = 5.0
+        self.min_speed = 3.0
+        self.vel = pygame.Vector2(random.uniform(-2.0, 2.0), random.uniform(-2.0, 2.0))
+        self.acc = pygame.Vector2()
         self.angle = 0
+        self.perception_radius = 25
+        self.max_alignment = 0.1
 
-    def draw(self, screen):
-        screen.blit(self.img, self.pos)
+    def alignment(self):
+        mate_velocities = []
+        for boid in self.school:
+            if self.pos.distance_to(boid.pos) > self.perception_radius:
+                continue
+            if boid == self:
+                continue
+            mate_velocities.append(boid.vel)
+        if mate_velocities:
+            avg_velocity = pygame.Vector2(0.0, 0.0)
+            for vel in mate_velocities:
+                avg_velocity += vel
+            avg_velocity = avg_velocity / len(mate_velocities)
+            accel = avg_velocity - self.vel
+            if accel.magnitude() > 0:
+                accel = accel.clamp_magnitude(self.max_alignment)
+            return accel
+        return pygame.Vector2(0.0, 0.0)
 
-    def move(self):
-        self.pos = (self.pos[0] + self.dir[0], self.pos[1] + self.dir[1])
+    def cohesion(self):
+        pass
 
-    def update(self, screen):
-        self.move()
-        self.update_angle()
-        self.draw(screen=screen)
-        
+    def separation(self):
+        pass
 
-    def update_angle(self):
-        magnitude = math.sqrt(self.dir[0]*2 + self.dir[1]*2)
-        mag_x = self.dir[0] / magnitude
-        mag_y = self.dir[1] / magnitude
-
-        # Calculate the angle (in degrees)
-        self.angle = math.degrees(math.atan2(-mag_x, mag_y)) - 90
-        print(self.angle)
-        pygame.transform.rotate(self.img, self.angle)
+    def perception(self):
+        pass
+    
+    def update(self):
+        self.acc = self.alignment()
+        self.vel = self.vel + self.acc
+        self.vel = self.vel.clamp_magnitude(self.min_speed, self.max_speed)
+        self.pos = self.pos + self.vel
+        if self.pos.x > self.screen.get_width():
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = self.screen.get_width()
+        if self.pos.y > self.screen.get_height():
+            self.pos.y = 0
+        if self.pos.y < 0:
+            self.pos.y = self.screen.get_height()
+        pygame.draw.circle(self.screen, (255, 255, 255), self.pos, 5)
+        pass
--- Boids/main.py
+++ Boids/main.py
@@ -6,29 +6,21 @@ pygame.init()
 IMAGE_PATH = r'Boids\boid\image\Boid.png'
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 800
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-BOID = Boid() 
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
+SCHOOL = []
+NUM_BOIDS = 100
+for i in range(NUM_BOIDS):
+    boid = Boid(screen, SCHOOL)
+    SCHOOL.append(boid) 
 
 clock = pygame.time.Clock()
 run = True
 pos = (100.0, 100.0)
+
 while run:
     screen.fill((0, 0, 0))
-    dx = 1.0
-    dy = 1.0
-    img = pygame.image.load(IMAGE_PATH).convert()
-    
-    magnitude = math.sqrt(dx**2 + dy**2)
-    dx /= magnitude
-    dy /= magnitude
-
-    # Calculate the angle (in degrees)
-    angle = math.degrees(math.atan2(-dy, dx)) - 90
-    
-    img = pygame.transform.rotate(img, angle)
-
-    pos = (pos[0] + dx, pos[1] + dy)
-    screen.blit(img, (pos))
+    for boid in SCHOOL:
+        boid.update()
     pygame.display.flip()
     clock.tick(30)
 
