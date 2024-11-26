d5a3bbadf563821a3b4ce9d7270196f4708c9ee6
TomHanssens <91314727+TooHanss@users.noreply.github.com>
2024-11-27 08:29:43 +1100

    added falling behaviour

--- FallingSand/main.py
+++ FallingSand/main.py
@@ -7,6 +7,10 @@ ROWS = 15
 COLUMNS = 20
 MARGIN = 1
 CELL_SIZE = 20 
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+
 SCREEN_WIDTH = COLUMNS * CELL_SIZE
 SCREEN_HEIGHT = ROWS * CELL_SIZE
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT)) 
@@ -18,16 +22,44 @@ for row in range(ROWS):
     for column in range(COLUMNS):
         grid[row].append(0)
 
+grid[1][1] = 1
+
 while run:
     screen.fill((255, 255, 255))
+    next_grid = []
+    for row in range(ROWS):
+        next_grid.append([])
+        for column in range(COLUMNS):
+            next_grid[row].append(0)
+
+
     for row in range(ROWS):
         for column in range(COLUMNS):
-            pygame.draw.rect(screen, (0, 0, 0),[(CELL_SIZE * column) + MARGIN,
+            if grid[row][column] == 1:
+                if row+1 == ROWS:
+                    next_grid[row][column] = 1
+                    continue
+                if grid[row+1][column] == 0:
+                    next_grid[row+1][column] = 1
+                    next_grid[row][column] = 0
+            if next_grid[row][column] == 1:        
+                color = WHITE
+            else:
+                color = BLACK
+            
+    for row in range(ROWS):
+        for column in range(COLUMNS):        
+            if next_grid[row][column] == 1:        
+                color = WHITE
+            else:
+                color = BLACK
+            pygame.draw.rect(screen, (color),[(CELL_SIZE * column) + MARGIN,
                                                 (CELL_SIZE * row) + MARGIN,
                                                 CELL_SIZE - (MARGIN * 2),
                                                 CELL_SIZE - (MARGIN * 2)]) # X pos, Y pos, width, height
+    grid = next_grid
     pygame.display.flip()
-    clock.tick(30)
+    clock.tick(10)
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
